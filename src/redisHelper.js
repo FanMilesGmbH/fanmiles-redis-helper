28149ac0694a63b12875c031a7b5ad06edcf815c
Miha Eržen <miha.erzen1@gmail.com>
2017-02-13 17:20:21 +0100

    Fix a bug where wrong configuration has been used

--- src/redisHelper.js
+++ src/redisHelper.js
@@ -72,7 +72,7 @@ module.exports = {
     getWriteEvent,
     getGetEvents,
     getInstances: (config) => {
-        const client = redis.createClient(config);
+        const client = redis.createClient(config.clientConfig);
 
         const dependencies = {
             client,
